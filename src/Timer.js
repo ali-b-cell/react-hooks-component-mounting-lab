02ceeb7975a4b1d865721c743b43fee13eb9e93e
alice <alice.nthenge@student.moringaschool.com>
2025-04-27 19:15:34 +0300

    Complete Component Mounting and Unmounting Lab

--- src/Timer.js
+++ src/Timer.js
@@ -6,29 +6,29 @@ class Timer extends Component {
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // componentDidMount sets up the interval to call clockTick every second
+  
   componentDidMount() {
     this.interval = setInterval(this.clockTick, 1000);
   }
 
-  // componentWillUnmount clears the interval to prevent memory leaks
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
-  // Updates the state every second
+  
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
     }));
   };
 
-  // Stops the clock by clearing the interval
+  
   stopClock = () => {
     clearInterval(this.interval);
   };
 
-  // Removes this timer from the list in App.js
+  
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
